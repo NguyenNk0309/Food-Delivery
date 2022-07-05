ddea3c9f19b68f2572dc8575a0a3bfd2a9992f47
NK <khoinguyen.030901@gmail.com>
2022-07-05 09:58:02 +0700

    Third commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { Header } from './components'
 
 const App = () => {
-    return (
-        <div className='w-screen h-auto flex flex-col'>
-            <Header />
-        </div>
-    )
+	return (
+		<div className="w-screen h-auto flex flex-col">
+			<Header />
+		</div>
+	)
 }
 
 export default App
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
 export const Header = () => {
-    return (
-        <div className='fixed z-50 w-screen bg-slate-300 p-6 px-16'>
-            {/* PC & Tablet */}
-            <div className='hidden md:flex w-full h-full'></div>
+	return (
+		<div className="fixed z-50 w-screen  p-6 px-16">
+			{/* PC & Tablet */}
+			<div className="hidden md:flex w-full h-full"></div>
 
-            {/* Mobile */}
-            <div className='flex md:hidden w-full h-full'></div>
-        </div>
-    )
+			{/* Mobile */}
+			<div className="flex md:hidden w-full h-full"></div>
+		</div>
+	)
 }
 
 export default Header
